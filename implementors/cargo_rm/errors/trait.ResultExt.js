9166e77ba4aa34b10b37076293f16f894c501673
Travis CI User <travis@example.org>
2020-02-07 10:44:14 +0000

    Documentation for killercup/cargo-edit@b8a8474

---
+++ implementors/cargo_rm/errors/trait.ResultExt.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["cargo_rm"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
